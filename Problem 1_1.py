236fd4c6da1a731ec0b84237f3704a3d7c052a30
Steinar Slatten <steinar.slatten@gmail.com>
2019-11-05 10:52:48 +0100

    Ferdig problem 1_1

--- Problem 1_1.py
+++ Problem 1_1.py
@@ -20,12 +20,16 @@ run_main = True # Defining boolean variable to control while loop
 
 while run_main: # Starting while loop
     try:        # Using try to test valid user input
-        user_input = input("Enter the number you want to " +        # Get and store user input
+        # Get and store user input
+        user_input = input("Enter the number you want to " +
             "know the English word for (0, 10, 20...100): ")
-        dict_value = my_dict[user_input]                            # Look up user input as key in dictionary
-    except KeyError:                                                # Raise error if input is not valid key in dict
-        print("That is not a valid number. Please try again.")      # Print error message
-    else:                                                           # If valid key in dictionary
-        print(f"The English word for {user_input} is {dict_value}") # Print result
-        run_main = False                                            # Turn off while loop for main program
+        dict_value = my_dict[user_input] # Look up input as key in dict
+    # Raise error if input is not valid key in dict:
+    except KeyError:
+        # Print error message
+        print("Sorry, the input was not valid.")
+    else:  # If valid key in dictionary
+        # Print result
+        print(f"The English word for {user_input} is {dict_value}")
+        run_main = False # Turn off while loop for main program
 
