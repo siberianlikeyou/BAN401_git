2039775034934445deee39df552e87216bb2e23a
Steinar Slatten <steinar.slatten@gmail.com>
2019-11-08 10:25:00 +0100

    Problem 1_2 helt ferdig

--- Problem 1_2.py
+++ Problem 1_2.py
@@ -1,10 +1,6 @@
 """
 Problem 1.2
 """
-# Get
-testString = "Write Python Code    a sequence word each  word can " \
-             "be Separated another word  Number   of white spaces" \
-             "    a write"
 
 # Get input sequence and split input. Convert this into set,
 # then convert to list and sort this list.
